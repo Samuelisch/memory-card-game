75445d0702fb2bfa34c82b40bcd8aca70d9baf4a
Samuelisch <samuelchanwk@hotmail.com>
2021-11-24 16:19:12 +0800

    added global style for app, bundled Header and Main

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import react from 'React'
+import React from 'react'
+import Header from './components/Header';
+import Main from './components/Main';
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+  :root,
+  body {
+    font-family: Helvetica;
+    font-size: 10px;
+    margin: 0;
+    background: rgb(0, 20, 60);
+  }
+`;
 
 const App = () => {
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <GlobalStyle />
+      <Header />
+      <Main />
     </div>
   );
 }
