96169f6f876cef8d51d86bb5c9b50c52f4328ae4
Samuelisch <samuelchanwk@hotmail.com>
2021-11-25 23:53:21 +0800

    removed console.log

--- src/components/gameAssets/f1DriverDeck.js
+++ src/components/gameAssets/f1DriverDeck.js
@@ -3,7 +3,6 @@ import { v4 as uuidv4 } from 'uuid'
 function importAll(r) {
   let images = {};
   r.keys().map((item, index) => images[item.replace('./', '')] = r(item));
-  console.log(Object.keys(images))
   return images;
 }
 
